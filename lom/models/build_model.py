813dc68a973c1f6c01be90648102a12c6ad97538
Juzezhang <juzezhang@gmail.com>
2025-04-19 10:55:28 -0700

    clean code

--- lom/models/build_model.py
+++ lom/models/build_model.py
@@ -1,8 +1,9 @@
 from omegaconf import OmegaConf
 from lom.config import instantiate_from_config
 
-def build_model(cfg, datamodule):
+# def build_model(cfg, datamodule):
+def build_model(cfg):
     model_config = OmegaConf.to_container(cfg.model, resolve=True)
     model_config['params']['cfg'] = cfg
-    model_config['params']['datamodule'] = datamodule
+    # model_config['params']['datamodule'] = datamodule
     return instantiate_from_config(model_config)
